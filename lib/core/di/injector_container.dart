9c857e964d5c438d3c5ae5de0457e548f16bae90
asafruddin022 <asafruddin022@gmail.com>
2021-11-02 15:44:23 +0700

    update some component

--- lib/core/di/injector_container.dart
+++ lib/core/di/injector_container.dart
@@ -13,7 +13,10 @@ import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
 Future<void> init() async {
+  /// initialisation local storage
   final prefs = await SharedPrefs.getInstance();
+
+  /// initialization graphql services [network]
   final gqlService = GraphQlServices();
 
   ///register local storage
